91df53627e45dd6e272c06cd5c1528e85e861259
Gavin Campbell <campbell_gav@yahoo.co.uk>
2025-02-08 18:21:06 +0000

    More level changes and updates. Set max levels to 5

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -184,7 +184,6 @@ func get_input() -> void:
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("hop") == true:
-			#velocity.y = JUMP_VELOCITY
 			jump_charge_timer.start()
 		if Input.is_action_just_released("hop") == true:
 			var timeElapsed = jump_charge_timer.wait_time - jump_charge_timer.time_left
